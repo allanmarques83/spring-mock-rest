bf98830c97b5e67efe573425b4382ed7eb89c5f8
Allan Marques <graftual@gmail.com>
2022-06-28 19:24:14 -0300

    fix rest interceptor

--- src/main/java/io/github/allanmarques83/RestInterceptorConfig.java
+++ src/main/java/io/github/allanmarques83/RestInterceptorConfig.java
@@ -1,13 +1,17 @@
 package io.github.allanmarques83;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import io.github.allanmarques83.RestInterceptor;
 
 @Configuration
+@EnableWebMvc
 public class RestInterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        System.out.println("passou aqui");
         registry.addInterceptor(new RestInterceptor());
     }
 }
