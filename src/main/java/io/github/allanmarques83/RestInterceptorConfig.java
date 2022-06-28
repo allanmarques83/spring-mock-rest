cfe697a43da53c35e3a6c1c289f8a9beaae2a58a
Allan Marques <graftual@gmail.com>
2022-06-28 18:41:22 -0300

    change rest interceptor config

--- src/main/java/io/github/allanmarques83/RestInterceptorConfig.java
+++ src/main/java/io/github/allanmarques83/RestInterceptorConfig.java
@@ -1,17 +1,13 @@
 package io.github.allanmarques83;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class RestInterceptorConfig implements WebMvcConfigurer {
-    @Autowired RestInterceptor restInterceptor;
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(restInterceptor).order(Ordered.LOWEST_PRECEDENCE);
+        registry.addInterceptor(new RestInterceptor());
     }
 }
