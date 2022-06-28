2d39159ffa920c0ddcc2cf0d3d9b6e2c4a1272a2
Allan Marques <graftual@gmail.com>
2022-06-28 19:43:45 -0300

    fix enable mocks

--- src/main/java/io/github/allanmarques83/EnableRestMockConfig.java
+++ src/main/java/io/github/allanmarques83/EnableRestMockConfig.java
@@ -3,10 +3,11 @@ package io.github.allanmarques83;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class EnableRestMockConfig implements DeferredImportSelector {
+public class EnableRestMockConfig implements ImportSelector {
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
         System.out.println("passou aqui0");
